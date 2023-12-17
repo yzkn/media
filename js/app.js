e3ccb9fe32f113099dcda9b1b684135be488d8b2
YA <9039417+yzkn@users.noreply.github.com>
2023-12-17 19:35:10 +0900

    Updates

--- js/app.js
+++ js/app.js
@@ -2,7 +2,47 @@
 
 
 window.addEventListener('DOMContentLoaded', _ => {
-    document.getElementById('openAudio').addEventListener('click', (event) => {
-        console.log(event);
+    document.getElementById('openAudio').addEventListener('change', (event) => {
+        if (event.target.files.length > 0) {
+            const file = event.target.files[0];
+            if (file.type.match('audio.*')) {
+                const reader = new FileReader();
+                reader.onload = () => {
+                    const audio = document.getElementById('audio');
+                    audio.src = reader.result;
+                    audio.style.display = 'inline';
+                };
+                reader.readAsDataURL(file);
+            }
+        }
+    });
+
+    document.getElementById('openImage').addEventListener('change', (event) => {
+        if (event.target.files.length > 0) {
+            const file = event.target.files[0];
+            if (file.type.match('image.*')) {
+                const reader = new FileReader();
+                reader.onload = () => {
+                    console.log('reader', reader);
+                    const image = document.getElementById('image');
+                    image.src = reader.result;
+                };
+                reader.readAsDataURL(file);
+            }
+        }
+    });
+
+    document.getElementById('openVideo').addEventListener('change', (event) => {
+        if (event.target.files.length > 0) {
+            const file = event.target.files[0];
+            if (file.type.match('video.*')) {
+                const reader = new FileReader();
+                reader.onload = () => {
+                    const video = document.getElementById('video');
+                    video.src = reader.result;
+                };
+                reader.readAsDataURL(file);
+            }
+        }
     });
 });
