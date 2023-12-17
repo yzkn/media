60d879e837c8066430bb01b4ca019569e90f2e1e
YA <9039417+yzkn@users.noreply.github.com>
2023-12-17 19:12:34 +0900

    Updates

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # media
- HTML5でメディアファイルを扱う
+
+HTML5でメディアファイルを扱う
+
+---
+
+Copyright (c) 2023 YA-androidapp(https://github.com/yzkn) All rights reserved.
---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023 YA-androidapp(https://github.com/yzkn) All rights reserved.
+
+
+window.addEventListener('DOMContentLoaded', _ => {
+    document.getElementById('openAudio').addEventListener('click', (event) => {
+        console.log(event);
+    });
+});
